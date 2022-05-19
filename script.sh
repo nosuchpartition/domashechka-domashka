ed67e045be197088afe805eb6a46fc15bc2a897d
nosuchpartition <neverstonegrey@gmail.com>
2022-05-19 11:47:53 +0300

    test

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cd git-project/
+mkdir folder folder1 folder2
+cd folder
+touch text.txt text1.txt text2.txt 535.json 526.json
+mkdir lesson1 lesson2 lesson3
+ls
+mv text.txt text1.txt lesson1
+# или так) mv C:/Users/Username/git-project/folder/text.txt text1.txt C:/Users/Username/git-project/folder/lesson1
+
+# а вообще вы крутые ребята, спасибо вам)
